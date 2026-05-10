cc97bc6c8eef6c3c61f516b091d4f16ca25dd9a4
PRAFUL33290 <contact@praful-design.fr>
2026-05-10 13:12:10 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-document.querySelectorAll('.fill').forEach(bar=>{const width=bar.style.width;bar.style.width='0';setTimeout(()=>{bar.style.width=width;},300);});
+document.querySelectorAll('.fill').forEach(bar=>{
+    const width = bar.style.width;
+    bar.style.width = '0';
+    setTimeout(()=>{
+        bar.style.width = width;
+    }, 300);
+});
